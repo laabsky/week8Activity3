59f2ad7ed7826647518169def87ae5512f03f7d4
Laabsky <jlaabs@madisoncollege.edu>
2018-03-19 20:27:08 -0500

    Activity 3 Completed

---
+++ src/test/java/gov/noaa/NdfdXMLBindingStubTest.java
@@ -0,0 +1,22 @@
+package gov.noaa;
+
+import org.junit.jupiter.api.Test;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Unmarshaller;
+import java.io.StringReader;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NdfdXMLBindingStubTest {
+
+    @Test
+    void latLonListZipCode() throws Exception {
+        NdfdXMLBindingStub binding = (NdfdXMLBindingStub) new NdfdXMLLocator().getndfdXMLPort();
+        String result = binding.latLonListZipCode("53562");
+        JAXBContext jaxbContext = JAXBContext.newInstance(DwmlType.class);
+        Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+        DwmlType dwml = (DwmlType) jaxbUnmarshaller.unmarshal(new StringReader(result));
+        assertEquals("43.1029,-89.5063", dwml.getLatLonList());
+    }
+}
